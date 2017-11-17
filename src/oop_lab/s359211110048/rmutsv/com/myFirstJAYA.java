e59e51fb3d2afae7fc45e99ca6b60ac32387567c
notmylovenot <notmylovenot@gmail.com>
2017-11-17 11:13:59 +0700

    Successfuly information data

--- src/oop_lab/s359211110048/rmutsv/com/myFirstJAYA.java
+++ src/oop_lab/s359211110048/rmutsv/com/myFirstJAYA.java
@@ -3,6 +3,9 @@ package oop_lab.s359211110048.rmutsv.com;
 public class myFirstJAYA {
     public static void main(String[] args) {
         System.out.println("Hello wordl, I'm Khathayut");
+        System.out.println("Informafion System");
+        System.out.println("Faculty of Management Technololgy");
+        System.out.println("RMUTSV");
     }//main
 
 }//class
