2f7494c9c64f3869546bf926416fc3f8721117b3
notmylovenot <notmylovenot@gmail.com>
2017-11-17 10:32:03 +0700

    Initial commit

---
+++ src/oop_lab/s359211110048/rmutsv/com/myFirstJAYA.java
@@ -0,0 +1,8 @@
+package oop_lab.s359211110048.rmutsv.com;
+
+public class myFirstJAYA {
+    public static void main(String[] args) {
+        System.out.println("Hello wordl, I'm Khathayut");
+    }//main
+
+}//class
