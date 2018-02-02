0417caa36770bf1716d2cf54f963151d046746df
notmylovenot <notmylovenot@gmail.com>
2018-02-02 11:21:58 +0700

    Pet,Name

---
+++ src/ooplab8/Rectangle.java
@@ -0,0 +1,25 @@
+package ooplab8;
+
+public class Rectangle implements Shape {
+    private double width;
+    private double length;
+
+    public Rectangle(double width, double length) {
+        this.width = width;
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    @Override
+    public double findArea() {
+     return width * length;
+
+    }
+}//class
